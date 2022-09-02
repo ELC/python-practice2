3cf854c8abc8f1e43734518e8917835cd713722e
IsTincho <segninitincho@gmail.com>
2022-09-02 15:46:08 -0300

    Delete tempCodeRunnerFile.py

--- exercises/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def sumatoria_sum(n: int) -> int:
-    """Re-Escribir utilizando la función sum.
-
-    Restricción: No utilizar bucles (FOR, WHILE, etc)
-    Referencia: https://docs.python.org/3/library/functions.html#sum
-    """
-    suma=sum(n, 1)
-    
-    print(str (suma))
